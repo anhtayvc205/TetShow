e73bf25793d68c8cc1a9baa63d1158ab057e9352
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-24 19:21:20 +0700

    Update ShowData.java

--- src/main/java/me/tetshow/data/ShowData.java
+++ src/main/java/me/tetshow/data/ShowData.java
@@ -1,19 +1,9 @@
 package me.tetshow.data;
 
-import me.tetshow.TetShow;
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class ShowData {
-    public Set<Color> colors = new HashSet<>();
-    public FireworkEffect.Type pattern = FireworkEffect.Type.BALL;
-    public int power = 2;
-
-    public ShowData(TetShow plugin) {
-        colors.add(Color.RED);
-        colors.add(Color.YELLOW);
-    }
+    public static Color color = Color.RED;
+    public static FireworkEffect.Type pattern = FireworkEffect.Type.BALL;
 }
