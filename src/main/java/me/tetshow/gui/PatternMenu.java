28e3d79a850d5412108964c524de14ba42da98c0
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-24 19:31:43 +0700

    Refactor PatternMenu for improved clarity

--- src/main/java/me/tetshow/gui/PatternMenu.java
+++ src/main/java/me/tetshow/gui/PatternMenu.java
@@ -9,28 +9,24 @@ import org.bukkit.inventory.meta.FireworkEffectMeta;
 public class PatternMenu {
 
     public static void open(Player p) {
-        Inventory inv = Bukkit.createInventory(null, 27, "§bChọn kiểu pháo");
+        Inventory inv = Bukkit.createInventory(null, 27, "§bPattern");
 
-        add(inv, 10, FireworkEffect.Type.BALL);
-        add(inv, 11, FireworkEffect.Type.BALL_LARGE);
-        add(inv, 12, FireworkEffect.Type.STAR);
-        add(inv, 13, FireworkEffect.Type.BURST);
-        add(inv, 14, FireworkEffect.Type.CREEPER);
+        add(inv, 11, FireworkEffect.Type.BALL);
+        add(inv, 13, FireworkEffect.Type.STAR);
+        add(inv, 15, FireworkEffect.Type.CREEPER);
 
         p.openInventory(inv);
     }
 
-    private static void add(Inventory inv, int slot, FireworkEffect.Type type) {
+    private static void add(Inventory inv, int slot, FireworkEffect.Type t) {
         ItemStack i = new ItemStack(Material.FIREWORK_STAR);
         FireworkEffectMeta meta = (FireworkEffectMeta) i.getItemMeta();
-        meta.setDisplayName("§a" + type.name());
-        meta.setEffect(FireworkEffect.builder().with(type).build());
+        meta.setEffect(FireworkEffect.builder().with(t).build());
         i.setItemMeta(meta);
         inv.setItem(slot, i);
     }
 
-    public static void select(Player p, FireworkEffect.Type type) {
-        ShowData.pattern = type;
-        p.sendMessage("§aĐã chọn kiểu pháo!");
+    public static void select(FireworkEffect.Type t) {
+        ShowData.pattern = t;
     }
 }
