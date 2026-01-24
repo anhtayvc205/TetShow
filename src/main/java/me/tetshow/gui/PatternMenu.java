d74a33391fcc9c88eefb90e8157c79216b650734
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-24 19:22:25 +0700

    Update display names and messages in PatternMenu

--- src/main/java/me/tetshow/gui/PatternMenu.java
+++ src/main/java/me/tetshow/gui/PatternMenu.java
@@ -9,7 +9,7 @@ import org.bukkit.inventory.meta.FireworkEffectMeta;
 public class PatternMenu {
 
     public static void open(Player p) {
-        Inventory inv = Bukkit.createInventory(null, 27, "§bChọn kiểu pháo hoa");
+        Inventory inv = Bukkit.createInventory(null, 27, "§bChọn kiểu pháo");
 
         add(inv, 10, FireworkEffect.Type.BALL);
         add(inv, 11, FireworkEffect.Type.BALL_LARGE);
@@ -23,7 +23,7 @@ public class PatternMenu {
     private static void add(Inventory inv, int slot, FireworkEffect.Type type) {
         ItemStack i = new ItemStack(Material.FIREWORK_STAR);
         FireworkEffectMeta meta = (FireworkEffectMeta) i.getItemMeta();
-        meta.setDisplayName("§e" + type.name());
+        meta.setDisplayName("§a" + type.name());
         meta.setEffect(FireworkEffect.builder().with(type).build());
         i.setItemMeta(meta);
         inv.setItem(slot, i);
@@ -31,6 +31,6 @@ public class PatternMenu {
 
     public static void select(Player p, FireworkEffect.Type type) {
         ShowData.pattern = type;
-        p.sendMessage("§aĐã chọn kiểu: " + type.name());
+        p.sendMessage("§aĐã chọn kiểu pháo!");
     }
 }
