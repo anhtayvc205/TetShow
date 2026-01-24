3c1eca4d1e789949708632b309bfe93433267aee
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-24 19:26:35 +0700

    Add colors list and initialize with RED
    Added a list of colors for fireworks and initialized it with RED.

--- src/main/java/me/tetshow/data/ShowData.java
+++ src/main/java/me/tetshow/data/ShowData.java
@@ -2,8 +2,21 @@ package me.tetshow.data;
 
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ShowData {
-    public static Color color = Color.RED;
+
+    // màu pháo
+    public static List<Color> colors = new ArrayList<>();
+
+    // kiểu pháo
     public static FireworkEffect.Type pattern = FireworkEffect.Type.BALL;
+
+    // độ cao pháo
+    public static int power = 2;
+
+    static {
+        colors.add(Color.RED);
+    }
 }
