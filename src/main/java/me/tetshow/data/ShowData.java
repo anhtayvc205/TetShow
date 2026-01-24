360cee9588aafdbf1866a2f9372ab56a150df831
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-24 19:28:53 +0700

    Optimize imports and clean up ShowData class
    Refactored imports and removed unnecessary ArrayList declaration.

--- src/main/java/me/tetshow/data/ShowData.java
+++ src/main/java/me/tetshow/data/ShowData.java
@@ -2,18 +2,12 @@ package me.tetshow.data;
 
 import org.bukkit.Color;
 import org.bukkit.FireworkEffect;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ShowData {
 
-    // màu pháo
     public static List<Color> colors = new ArrayList<>();
-
-    // kiểu pháo
     public static FireworkEffect.Type pattern = FireworkEffect.Type.BALL;
-
-    // độ cao pháo
     public static int power = 2;
 
     static {
